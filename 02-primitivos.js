8c700f8a146c6c88d33b1c0e493abb03acfab554
Jurgen <belenlopezjurgen@gmail.com>
2024-05-24 21:39:46 -0400

    mi segundo comentario

---
+++ 02-primitivos.js
@@ -0,0 +1,13 @@
+/* variables primitivas*/
+/*number*/
+let numero = 1;
+/*String*/
+let texto = "Hola Mundo";
+/*Boolean*/
+let verdadero = true;
+let falso = false;
+/*undefined*/
+let noDefinido;
+let undef = undefined;
+/*null*/
+let nulo = null; 
---
+++ 05-comentarios.js
@@ -0,0 +1,12 @@
+/* 
+Todo lo que escribamos entremedio de los slash y los
+asteriscos va a ser ignorado
+*/
+/**
+ * Este es un comentario
+ * de muchas lineas
+ * muuuchas lineas
+ */
+let number = 42;// Este es otro comentario
+
+console.log(number);
